754cc3f80664c9d74b2cb75328920b69ce5633f2
siddharth-bhatnagar <siddharth.bhatnagar.msit@iosd.tech>
2020-12-09 19:48:14 +0530

    done cleanup and modified file structure

---
+++ amazon-clone/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./css/App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, let's build the Amazon Store! 🚀</h1>
+
+        { /* Header */ }
+        { /* Home */ }
+    </div>
+  );
+}
+
+export default App;
---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAt8Wum1YVHRTgXaxafd35jD2247EfYyzg",
+  authDomain: "clone-4b45e.firebaseapp.com",
+  projectId: "clone-4b45e",
+  storageBucket: "clone-4b45e.appspot.com",
+  messagingSenderId: "30992702380",
+  appId: "1:30992702380:web:9da0aadc0d4cce8ed87809",
+  measurementId: "G-1WDVX0QRFY"
+};
