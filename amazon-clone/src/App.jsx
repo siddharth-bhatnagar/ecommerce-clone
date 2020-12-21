52907841dc8c6d5f06d97cb31a52f9ebca77cbdc
siddharth-bhatnagar <siddharth.bhatnagar.msit@iosd.tech>
2020-12-21 23:38:50 +0530

    Created checkout component, update pending...

--- amazon-clone/src/App.jsx
+++ amazon-clone/src/App.jsx
@@ -3,18 +3,18 @@ import Header from "./components/Header/Header";
 import "./css/App.css";
 import Home from "./components/Home/Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Checkout from "./components/Checkout/Checkout";
 
 function App() {
   return (
     <Router>
       <div className="app">
+        <Header />
         <Switch>
           <Route path="/checkout">
-            <Header />
-            <p>I AM Checkout</p>
+            <Checkout />
           </Route>
           <Route path="/">
-            <Header />
             <Home />
           </Route>
         </Switch>
